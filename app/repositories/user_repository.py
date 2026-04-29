859ddb310cc64c74b51e8b9bc7801e61236f675d
Yohan Grondin <yohan.grondin@outlook.com>
2026-04-29 19:50:26 +0200

    feat(user): modifying/update repository

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,63 +1,96 @@
-from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from app.models.tables.user import User
-from app.schemas.user_schema import UserCreate, UserUpdate
-from typing import Any, List
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from typing import List
 
 class UserRepository:
+    """
+    User repository providing CRUD operations for a given SQLAlchemy model.
+    """
     def __init__(self, db: AsyncSession) -> None:
+        """
+        Initialize the User repository with database session.
+        Args:
+            db (AsyncSession): Asynchronous database session.
+        """
         self.db = db
 
     async def get_by_id(self, user_id: int) -> User | None:
         """
+        Retrieve a user instance by its ID
 
-        :param user_id:
-        :return:
+        Args:
+            user_id: Yhe unique identifier of the user.
+        Returns:
+            User | None: The user instance if found, otherwise None.
         """
         return await self.db.get(User, user_id)
 
     async def get_by_email(self, email: str) -> User | None:
+        """
+        Retrieve a user instance by its email.
+        Args:
+            email (str): the email of user.
+        Returns:
+             User | None: The user instance if found, otherwise None.
+        """
         result = await self.db.execute(select(User).where(User.email == email))
         return result.scalars().one_or_none()
 
     async def get_by_username(self, username: str) -> User | None:
+        """
+        Retrieve a user instance by its username.
+        Args:
+            username (str): The username of user.
+        Returns:
+             User | None: The user instance if found, otherwise None.
+        """
         result = await self.db.execute(select(User).where(User.username == username))
         return result.scalars().one_or_none()
 
-    async def get_all(self) -> List[User] | Any:
-        result = await self.db.execute(select(User))
-        return result.scalars().all()
+    async def get_all(self) -> List[User]:
+        """
+        Retrieve all instances of user from the database.
 
-    async def create(self, data: UserCreate) -> User:
+        Returns:
+            list[User]: The List of all user's instances.
+        """
+        return list(await self.db.scalars(select(User)))
 
-        hashed_password = pwd_context.hash(data.password)
+    async def create(self, data: dict) -> User:
+        """
+        Create a new user instance, with the password hashed
 
-        user = User(
-            email=data.email,
-            username=data.username,
-            firstname=data.firstname,
-            lastname=data.lastname,
-            phone=data.phone,
-            address=data.address,
-            password_hash=hashed_password,
-        )
+        Args:
+            data (dict): Schema containing fields to create the user instance.
+        Returns:
+             user(User): The new user instance.
+        """
+        user = User(**data)
         self.db.add(user)
         await self.db.commit()
         await self.db.refresh(user)
         return user
 
-    async def update(self,user_id: int, data: UserUpdate) -> User:
+    async def update(self,user_id: int, data: dict) -> User | None:
+        """
+        Update an existing user instance by its ID.
+
+        Args
+            user_id(int): The user ID to update.
+            data (UserUpdate): Schema containing fields to update the user instance.
+
+        Returns:
+             user(User): The updated user instance.
+        Side Effects:
+            Commits changes to the database and refreshes the user instance.
+        """
+
         user = await self.get_by_id(user_id)
-        if not user:
-            raise HTTPException(status_code=404, detail="User not found")
+        if user is None:
+            return None
 
-        updated_user = data.model_dump(exclude_unset=True)
-
-        for key, value in updated_user.items():
+        for key, value in data.items():
             setattr(user, key, value)
 
         await self.db.commit()
@@ -65,6 +98,15 @@ class UserRepository:
         return user
 
     async def delete(self, user_id: int) ->  bool:
+        """
+        Delete an existing user instance by its ID.
+        Args:
+            user_id(int): The user ID to delete.
+        Returns:
+             bool: True if the user instance was successfully deleted, False otherwise.
+        Side Effects:
+            Commits deletion to the database.
+        """
         user = await self.get_by_id(user_id)
         if not user:
             return False
