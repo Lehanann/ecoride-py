10e3dc9055d98dda207cbf2ee02995386962dc81
Yohan Grondin <yohan.grondin@outlook.com>
2026-04-29 19:59:01 +0200

    feat(main): add route on main

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from app.api.v1.endpoints import (user_router)
+
+app = FastAPI()
+app.include_router(user_router)
+
+
+@app.get("/")
+async def root():
+    """
+    Entrypoint ecoride backend fastapi
+
+    Returns: Welcome message
+    """
+    return {"message": "Welcome API EcoRide!"}
