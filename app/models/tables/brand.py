e6716d3f4e94edc85bdc8c532659ad5cd532634d
Yohan Grondin <yohan.grondin@outlook.com>
2026-04-30 18:38:32 +0200

    feat(car): Add Car entity and relationships with User and Brand

--- app/models/tables/brand.py
+++ app/models/tables/brand.py
@@ -5,7 +5,7 @@ from databases.postgresql import Base
 
 class Brand(Base):
     """
-    Represent a brand stored int the database.
+    Represent a brand stored in the database.
 
     Attributes:
         id (int): The id of the brand stored int the database.
@@ -19,4 +19,6 @@ class Brand(Base):
     __tablename__ = 'brands'
 
     id: Mapped[int]= mapped_column(Integer, primary_key=True, index=True)
-    name: Mapped[str]= mapped_column(String(50), index=True)
+    name: Mapped[str]= mapped_column(String(50), nullable=False, index=True)
+
+    cars = relationship("Car", back_populates="brand")
