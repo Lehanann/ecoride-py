88cfa9cb28660681b4d02cbd6d066c27db0f48e5
Yohan Grondin <yohan.grondin@outlook.com>
2026-05-07 16:41:37 +0200

    feat(car): update route with auth_middleware minimalist

--- app/api/v1/endpoints/car_route.py
+++ app/api/v1/endpoints/car_route.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Request
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.schemas.car_schema import CarRead, CarCreate, CarUpdate
+from app.schemas.car_schema import CarCreate, CarRead
 from app.services.car_service import CarService
 from app.repositories.car_repository import CarRepository
 from databases.postgresql import get_session
@@ -10,20 +10,30 @@ router = APIRouter(prefix="/car", tags=["car"])
 def get_service_car(db: AsyncSession = Depends(get_session)) -> CarService:
     return CarService(CarRepository(db))
 
-@router.get("/", response_model=list[CarRead])
-async def list_cars(service: CarService = Depends(get_session)):
-    return await service.get_all()
 
-@router.get("/{car_id}", response_model=CarRead)
-async def get_car(car_id: int, service: CarService = Depends(get_session)):
-    return await service.get_by_id(car_id)
+@router.get("/", response_model=list[CarRead])
+async def get_all_cars(request: Request, service: CarService = Depends(get_service_car)):
+    """
+
+    Args:
+        request:
+        service:
+
+    Returns:
+
+    """
+    user_id = request.state.user_id
+    return await service.get_all_cars_by_user(user_id)
 
 @router.post("/", response_model=dict[str,str], status_code=status.HTTP_201_CREATED)
-async def create_car(data: CarCreate, service: CarService = Depends(get_session)):
-    await service.create(data)
+async def create_car(request: Request, data: CarCreate, service: CarService = Depends(get_service_car)):
+    user_id = request.state.user_id
+    await service.create(data, user_id)
     return {"message": "car created successfully"}
 
-@router.patch("/{car_id}", response_model=dict[str,str], status_code=status.HTTP_200_OK)
-async def update_car(car_id: int , data: CarUpdate, service: CarService = Depends(get_session)):
-    await service.update(car_id, data)
-    return {"message": "car updated successfully"}
+@router.delete("/{car_id}",response_model=dict[str,str], status_code=status.HTTP_200_OK)
+async def delete_car(request: Request, car_id: int, service: CarService = Depends(get_service_car)):
+    user_id = request.state.user_id
+    await service.delete(car_id, user_id)
+    return {"message": "car deleted successfully"}
+
