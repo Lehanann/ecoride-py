f18d0a8cca5c43f35c5049fcadc67bd75b03159a
Yohan Grondin <yohan.grondin@outlook.com>
2026-05-28 18:37:39 +0200

    refactor(models): clean Role entity

--- app/models/tables/role.py
+++ app/models/tables/role.py
@@ -5,17 +5,23 @@ from app.models.association_tables import role_user
 
 class Role(Base):
     """
-     Represents a car stored in the database.
+     Represents a user role in the application (e.g. admin, user, employee).
+
      Attributes:
-         id (int): The ID of the role stored in the database.
-         name (str): The name of the role stored in the database.
+         id (int): The ID of the role.
+         name (str): The name of the role.
+
+     Relationships:
+        users: Users associated with the role.
+
      Notes:
+         - The name should be unique.
          - The name must not exceed 50 characters.
          - The name is required.
      """
     __tablename__ = 'roles'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    name: Mapped[str] = mapped_column(String(50), nullable=False, index=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False, unique=True, index=True)
 
     users = relationship("User", secondary=role_user,back_populates="roles")
