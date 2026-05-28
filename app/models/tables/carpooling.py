3221b6057a3d3df08a8365716f32a56096a48525
Yohan Grondin <yohan.grondin@outlook.com>
2026-05-28 18:34:40 +0200

    refactor(models): improve Carpooling model & docstring

--- app/models/tables/carpooling.py
+++ app/models/tables/carpooling.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Integer, String, Date, Time, ForeignKey, Numeric, text
+from sqlalchemy import Integer, String, Date, Time, ForeignKey, Numeric, text, DateTime, func, SmallInteger
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from databases.postgresql import Base
-from datetime import date, time
+from datetime import date, time, datetime
 from sqlalchemy.dialects.postgresql import ENUM
 from app.utils.carpooling_status_enum import CarpoolingStatusEnum
 from decimal import Decimal
@@ -9,6 +9,7 @@ from decimal import Decimal
 class Carpooling(Base):
     """
     Represents a carpooling stored in the database.
+
     Attributes:
         id (int): The ID of the carpooling. Ex.: 1
         departure_date (date): The date of the departure of the carpooling. Ex.: '2020-04-01' YYYY-MM-DD
@@ -18,9 +19,20 @@ class Carpooling(Base):
         end_time (time): The time of the end of the carpooling. Ex.: '07:40'
         end_location (str): The location of the end of the carpooling. Ex.: '39 Bis impasse des Secrets 69899 Le Bois-Enchanté'
         status (CarpoolingStatusEnum): The status of the carpooling initialized to draft by the db. Ex.: 'draft'
-        place_number (int): The place number available of the carpooling. Ex.: 2
+        place_number (int): Number of available seats. Ex.: 2
         price (Decimal): The price of the carpooling, minimum 2 credits. Ex.: 4
         car_id (int): The ID of the car used for the carpooling. Ex.: 12
+        created_at (datetime): The date when the carpooling was created. Managed by the db. Ex.: 2019-04-01T12:00:00Z
+        updated_at (datetime): The date when the carpooling was last updated. Managed by the db. Ex.: 2020-04-01T12:00:00Z
+
+    Relationships:
+        car: The car associated with the carpooling.
+        reservations: Reservations associated with the carpooling.
+        opinions: The opinions associated with the carpooling.
+    Notes:
+        - All user-provided fields are required.
+        - departure_location and end_location must not exceed 200 characters.
+        - price must not exceed 9999.99
     """
     __tablename__ = "carpoolings"
 
@@ -32,14 +44,19 @@ class Carpooling(Base):
     end_time: Mapped[time] = mapped_column(Time, nullable=False)
     end_location: Mapped[str] = mapped_column(String(200), nullable=False)
     status: Mapped[CarpoolingStatusEnum] = mapped_column(ENUM(CarpoolingStatusEnum,
-                                                              name="carpooling_status_enum",
+                                                              name="carpooling_status",
                                                               create_type=False),
                                                          nullable=False,
                                                          server_default=text("'draft'"))
-    place_number: Mapped[int] = mapped_column(Integer, nullable=False)
+    place_number: Mapped[int] = mapped_column(SmallInteger, nullable=False)
     price: Mapped[Decimal] = mapped_column(Numeric(6,2), nullable=False)
     car_id: Mapped[int] = mapped_column(Integer, ForeignKey('cars.id'), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, server_default=func.now())
 
+    # Relation many-to-one with Car entity
     car = relationship("Car", back_populates="carpoolings")
+    # Relation one-to-many with Reservation entity
     reservations = relationship("Reservation", back_populates="carpooling")
+    # Relation one-to-many with opinion Entity
     opinions = relationship("Opinion", back_populates="carpooling")
