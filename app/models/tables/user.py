980c6bdac9838f20b8ad4f3f6b2feff91eabe1ed
Yohan Grondin <yohan.grondin@outlook.com>
2026-05-28 18:36:09 +0200

    refactor(models): refactor User entity

--- app/models/tables/user.py
+++ app/models/tables/user.py
@@ -7,23 +7,34 @@ from app.models.association_tables import role_user
 
 class User(Base):
     """
-    Represent a user stored in the database
+    Represents a user in the application.
+    A user can have one or more roles (e.g. passenger, driver).
+    Field is_active allows the soft delete of the user.
+
     Attributes:
         id (int): The id of the user. Ex.: 1
         username (str): The username of the user. Ex.: 'jdoe'
         firstname (str): The first name of the user. Ex.: 'John'
         lastname (str): The last name of the user. Ex.: 'Doe'
         email (str): The email of the user. Ex.: 'jdoe@example.fr'
-        password_hash (str): The password of the user. Ex. 'dfhrzegge5r4g5reg45er4gr5e4rt4'
+        password_hash (str): Hashed password of the user. Ex. 'dfhrzegge5r4g5reg45er4gr5e4rt4'
         phone (str): The phone number of the user. Ex.: '0102030405'
-        address (str): The address of the user Ex.: '24 rue de la République 75001 Paris'
-        birth_date(date): Date of birth of the user.(YYYY-mm-dd) Ex: '1981-01-01'
-        avatar_url (str): Path photo of the user. Ex.: /storage/bb245fea5411zsz/e5rqd2d14.png
+        address (str): The address of the user. Ex.: '24 rue de la République 75001 Paris'
+        birth_date (date): Date of birth of the user. (YYYY-MM-DD) Ex: '1981-01-01'
+        avatar_url (str): Path to the user's profile picture. Ex.: /storage/bb245fea5411zsz/e5rqd2d14.png
         credit (Numeric): The credit of the user. Ex.: 20
-        created_at (Datetime): The date when the user was created.
-        updated_at (Datetime): The date when the user was last updated.
-        is_active (bool): Whether the user is active, default true. Ex. True
-        deleted_at (datetime): The date when the user was deleted. Used to soft delete.
+        created_at (datetime): The date when the user was created. Managed by the db.
+        updated_at (datetime): The date when the user was last updated. Managed by the db.
+        is_active (bool): Whether the user is active, default true. Ex.: True
+        deleted_at (datetime): The date when the user was deleted. Managed by the db.
+
+    Relationships:
+        cars: The cars associated to a user.
+        roles: The roles associated to a user.
+        reservations: The reservations associated to a user.
+        opinions_given: Opinions created by the user.
+        opinions_received: Opinions received by the user.
+        opinions_validated: Opinions validated by the user.
     """
 
     __tablename__ = "users"
@@ -39,10 +50,10 @@ class User(Base):
     birth_date: Mapped[date | None] = mapped_column(Date, nullable=True)
     avatar_url: Mapped[str | None] = mapped_column(String(254), nullable=True)
     credit: Mapped[Decimal] = mapped_column(Numeric(6, 2), nullable=False, server_default=text('20'))
-    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False,server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=False,server_default=func.now())
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False,server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False,server_default=func.now())
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, server_default=text('true'))
-    deleted_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    deleted_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=True)
 
     cars = relationship("Car", back_populates="user")
     roles = relationship("Role", secondary=role_user,back_populates="users")
