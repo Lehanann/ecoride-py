d4386f03564266878eb5e2dfbb6f2eb28d8d1cba
Yohan Grondin <yohan.grondin@outlook.com>
2026-05-28 13:40:47 +0200

    refactor: introduce soft delete with is_active fields and improve CRUD

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -2,6 +2,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from app.models.tables.user import User
 from typing import List
+from datetime import datetime, UTC
+
 
 class UserRepository:
     """
@@ -10,6 +12,7 @@ class UserRepository:
     def __init__(self, db: AsyncSession) -> None:
         """
         Initialize the User repository with database session.
+
         Args:
             db (AsyncSession): Asynchronous database session.
         """
@@ -24,28 +27,31 @@ class UserRepository:
         Returns:
             User | None: The user instance if found, otherwise None.
         """
-        return await self.db.get(User, user_id)
+        result = await self.db.execute(select(User).where(User.id == user_id).where(User.is_active == True))
+        return result.scalars().one_or_none()
 
     async def get_by_email(self, email: str) -> User | None:
         """
         Retrieve a user instance by its email.
+
         Args:
             email (str): the email of user.
         Returns:
              User | None: The user instance if found, otherwise None.
         """
-        result = await self.db.execute(select(User).where(User.email == email))
+        result = await self.db.execute(select(User).where(User.email == email).where(User.is_active == True))
         return result.scalars().one_or_none()
 
     async def get_by_username(self, username: str) -> User | None:
         """
         Retrieve a user instance by its username.
+
         Args:
             username (str): The username of user.
         Returns:
              User | None: The user instance if found, otherwise None.
         """
-        result = await self.db.execute(select(User).where(User.username == username))
+        result = await self.db.execute(select(User).where(User.username == username).where(User.is_active == True))
         return result.scalars().one_or_none()
 
     async def get_all(self) -> List[User]:
@@ -55,7 +61,7 @@ class UserRepository:
         Returns:
             list[User]: The List of all user's instances.
         """
-        return list(await self.db.scalars(select(User)))
+        return list(await self.db.scalars(select(User).where(User.is_active == True)))
 
     async def create(self, data: dict) -> User:
         """
@@ -93,19 +99,19 @@ class UserRepository:
 
         return user
 
-    async def delete(self, user_id: int) ->  bool:
+    async def delete(self, user_id: int) -> User| None:
         """
         Delete an existing user instance by its ID.
+
         Args:
             user_id(int): The user ID to delete.
+
         Returns:
              bool: True if the user instance was successfully deleted, False otherwise.
-        Side Effects:
-            Commits deletion to the database.
         """
         user = await self.get_by_id(user_id)
         if not user:
-            return False
-        await self.db.delete(user)
-        await self.db.commit()
-        return True
+            return None
+        user.is_active = False
+        user.deleted_at = datetime.now(UTC)
+        return user
