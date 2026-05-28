c55e1db33eab199c0e4d5b88e1456a6f6644e0a9
Yohan Grondin <yohan.grondin@outlook.com>
2026-05-28 18:38:10 +0200

    refactor(models): clean Transaction entity

--- app/models/tables/transaction.py
+++ app/models/tables/transaction.py
@@ -8,15 +8,21 @@ from databases.postgresql import Base
 
 class Transaction(Base):
     """
-    Represents a transaction in the database.
+    Represents a financial transaction related to a carpooling.
+    Each transaction records a debit or credit for a user.
+    It can represent a payment, a commission, or a refund.
 
     Attributes:
         id (int): The identifier of the transaction.
         amount (Decimal): The amount of the transaction.
         user_id (int): The user identifier of the transaction.
         carpooling_id (int): The carpooling identifier of the transaction.
-        type (TransactionTypeEnum): The transaction type of the transaction.
-        created_at (datetime): The date and time when the transaction was created.
+        transaction_type (TransactionTypeEnum): The type of the transaction (payment, commission, refund).
+        created_at (datetime): The date and time when the transaction was created. Managed by the db.
+
+    Relationships:
+        user: The user associated with the transaction.
+        carpooling: The related carpooling.
     """
     __tablename__ = 'transactions'
 
@@ -28,4 +34,4 @@ class Transaction(Base):
                                                            name='transaction_type',
                                                            create_type=False),
                                                       nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
