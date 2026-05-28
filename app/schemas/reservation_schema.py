ad64e516748dff9f17629438de50fcaa732f1fcc
Yohan Grondin <yohan.grondin@outlook.com>
2026-05-28 10:37:55 +0200

    docs: add dosctring in reservation schema

--- app/schemas/reservation_schema.py
+++ app/schemas/reservation_schema.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel, Field
 
 class ReservationCreate(BaseModel):
+    """
+    Schema used to create a new reservation.
 
+    Attributes:
+        carpooling_id (int): The Identifier of carpooling
+
+    Notes:
+        - The carpooling_id is required.
+    """
     carpooling_id: int = Field(..., description="The id of the carpooling that this reservation belongs to")
 
