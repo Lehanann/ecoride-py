bf6b5dbc816fe3962b6085a367ceb9dba16d9fa7
Yohan Grondin <yohan.grondin@outlook.com>
2026-06-04 17:41:03 +0200

    refactor(files): add file service for profile image handling

--- app/services/file_service.py
+++ app/services/file_service.py
@@ -1,10 +1,15 @@
 from pathlib import Path
 from uuid import uuid4
 from fastapi import UploadFile
+import logging
+
+logger = logging.getLogger(__name__)
 
 class FileService:
     """
+    Service responsible for handling profile image files.
 
+    Provides utilities for saving and replacing user profile images on disk.
     """
     STORAGE_DIR = Path.cwd() / 'storage/profiles'
     BUFFER_SIZE = 1024 * 1024
@@ -12,20 +17,19 @@ class FileService:
     @classmethod
     def save_profile_image(cls, file: UploadFile) -> str:
         """
+        Save a new profile image.
 
         Args:
-            file:
+            file (UploadFile): New uploaded file
 
         Returns:
-
+            str: The new file path
         """
         cls.STORAGE_DIR.mkdir(parents=True, exist_ok=True)
 
-        extension: str = file.filename.split('.')[-1]
+        extension: str = file.filename.split('.')[-1].lower()
         filename: str = f'{uuid4()}.{extension}'
         file_path = cls.STORAGE_DIR / filename
-
-        file_path.touch()
 
         with file_path.open('wb') as buffer :
             for chunk in iter(lambda: file.file.read(cls.BUFFER_SIZE), b''):
@@ -33,13 +37,27 @@ class FileService:
 
         return f'storage/profiles/{filename}'
 
-    def replace_profile_image(self, file: UploadFile) -> str:
+    @classmethod
+    def replace_profile_image(cls, file: UploadFile, old_file_path: str | None) -> str:
         """
+        Replace an existing profile image.
+        - Deletes the old file if it exists
+        - Saves the new file
 
         Args:
-            file:
+            file (UploadFile): New uploaded file
+            old_file_path (str | None): Existing file path
 
         Returns:
-
+            str: New file path
         """
 
+        if old_file_path:
+            try:
+                old_path = Path.cwd() / old_file_path
+                if old_path.exists() and old_path.is_file():
+                    old_path.unlink()
+            except Exception:
+                logger.exception("Unexpected error while removing old file path")
+
+        return cls.save_profile_image(file)
