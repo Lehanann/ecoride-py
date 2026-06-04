816067a1c6504c7c8655b14b4829e497ecb22ab9
Yohan Grondin <yohan.grondin@outlook.com>
2026-06-04 16:47:32 +0200

    refactor(opinion): improve validation logic and error handling

--- app/services/opinion_service.py
+++ app/services/opinion_service.py
@@ -1,5 +1,7 @@
+import logging
+from sqlalchemy.exc import IntegrityError
+from app.core.exceptions.http_exceptions import bad_request, forbidden, not_found, conflict
 from datetime import datetime, timezone
-from fastapi import HTTPException, status
 from app.repositories.opinion_repository import OpinionRepository
 from app.repositories.user_repository import UserRepository
 from app.repositories.carpooling_repository import CarpoolingRepository
@@ -8,11 +10,23 @@ from app.schemas.opinion_schema import OpinionCreate, OpinionStatusUpdate
 from app.utils.opinion_status_enum import OpinionStatusEnum
 from app.utils.carpooling_status_enum import CarpoolingStatusEnum
 
+logger = logging.getLogger(__name__)
+
 class OpinionService:
     """
     Service handling business logic related to opinions.
     Includes creation, validation, and retrieval workflows.
     """
+    USER_NOT_FOUND = "User not found"
+    CARPOOLING_NOT_FOUND = "Carpooling not found"
+    OPINION_NOT_FOUND = "Opinion not found"
+    OPINION_ALREADY_EXISTS = "Opinion already exists"
+    CARPOOLING_NOT_FINISHED = "Carpooling not finished"
+    NOT_EMPLOYEE = "User has not the 'employee' role"
+    OPINION_NOT_PENDING = "Opinion not pending"
+    OPINION_NOT_UPDATED = "Opinion not updated"
+    USER_NOT_PARTICIPANT = "User is not part of this carpooling"
+
     def __init__(self, opinion_repository: OpinionRepository, user_repository: UserRepository, carpooling_repository: CarpoolingRepository):
         """
         Initialize the service with required repositories.
@@ -43,7 +57,7 @@ class OpinionService:
             user_id (int): The unique identifier of the driver.
 
         Raises:
-            HTTPException:
+            not_found:
                 - If the user does not exist.
 
         Returns:
@@ -51,7 +65,7 @@ class OpinionService:
         """
         user = await self.user_repository.get_by_id(user_id)
         if user is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+            raise not_found(detail=self.USER_NOT_FOUND)
 
         return await self.opinion_repository.get_all_by_user(user_id)
 
@@ -70,29 +84,35 @@ class OpinionService:
             opinion (OpinionCreate): The data used to create the opinion.
 
         Raises:
-            HTTPException:
-                - If validation fails or a database error occurs.
+            not_found:
+                - If the user is not found.
+                - If the carpooling is not found.
+            forbidden:
+                - If the user does not part of this carpooling.
+            bad_request:
+                - If the carpooling is not finished.
+                - If an error occurs while creating the opinion.
 
         Returns:
             Opinion: The created opinion.
         """
         author = await self.user_repository.get_by_id(author_id)
         if author is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Author not found")
+            raise not_found(detail=self.USER_NOT_FOUND)
 
         carpooling = await self.carpooling_repository.get_by_id(opinion.carpooling_id)
         if carpooling is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Carpooling not found")
+            raise not_found(detail=self.CARPOOLING_NOT_FOUND)
         if carpooling.status != CarpoolingStatusEnum.finished:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Carpooling not finished")
+            raise bad_request(detail=self.CARPOOLING_NOT_FINISHED)
 
         target_id = carpooling.car.user_id
 
         if author_id == target_id:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You cannot review yourself")
+            raise bad_request(detail="You cannot review yourself")
 
         if author_id not in [p.user_id for p in carpooling.reservations]:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User not part of this carpooling")
+            raise forbidden(detail=self.USER_NOT_PARTICIPANT)
 
         opinion_data = {
             "comment": opinion.comment,
@@ -106,9 +126,14 @@ class OpinionService:
             opinion = await self.opinion_repository.create(opinion_data)
             await self.opinion_repository.db.commit()
             return opinion
+        except IntegrityError:
+            await self.opinion_repository.db.rollback()
+            logger.exception("Integrity error while creating a new opinion")
+            raise conflict(detail=self.OPINION_ALREADY_EXISTS)
         except Exception:
             await self.opinion_repository.db.rollback()
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+            logger.exception("Unexpected error while creating a new opinion")
+            raise bad_request(detail="Error creating opinion")
 
     async def validate_opinion(self, opinion_id: int, opinion: OpinionStatusUpdate, validator_id: int) -> Opinion:
         """
@@ -123,10 +148,17 @@ class OpinionService:
             validator_id (int): The unique identifier of the user validating the opinion.
 
         Raises:
-            HTTPException:
-                - 404 if user or opinion is not found
-                - 403 if user is not authorized
-                - 400 if opinion is not in pending state or update fails.
+            not_found:
+                - If the user is not found.
+                - If the user or opinion is not found.
+                - If the opinion was not updated.
+            forbidden:
+                - If the user has not the 'employee' role.
+            bad_request:
+                - If the opinion is not in pending state.
+                - If an error occurs while validating the opinion.
+            conflict:
+                - If the opinion already exists.
 
         Returns:
             Opinion: The updated opinion.
@@ -134,7 +166,7 @@ class OpinionService:
         validator = await self.user_repository.get_by_id(validator_id)
 
         if validator is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+            raise not_found(detail=self.USER_NOT_FOUND)
 
         # Checks that the user has the 'employee' role
         roles = validator.roles
@@ -145,16 +177,16 @@ class OpinionService:
                 break
 
         if not is_employee:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+            raise forbidden(detail=self.NOT_EMPLOYEE)
 
         # Checks current opinion exists
         current_opinion = await self.opinion_repository.get_by_id(opinion_id)
         if current_opinion is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Opinion not found")
+            raise not_found(detail=self.OPINION_NOT_FOUND)
 
         # checks current opinion is pending status.
         if current_opinion.status != OpinionStatusEnum.pending:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Opinion not pending")
+            raise bad_request(detail=self.OPINION_NOT_PENDING)
 
         try:
             updated_opinion = await self.opinion_repository.update_status(opinion_id,
@@ -162,12 +194,17 @@ class OpinionService:
                                                                           validator.id,
                                                                           validated_at=datetime.now(timezone.utc))
             if updated_opinion is None:
-                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Opinion not updated")
+                raise bad_request(detail=self.OPINION_NOT_UPDATED)
             await self.opinion_repository.db.commit()
             return updated_opinion
+        except IntegrityError:
+            await self.opinion_repository.db.rollback()
+            logger.exception("Integrity error while updating a new opinion")
+            raise conflict(detail=self.OPINION_ALREADY_EXISTS)
         except Exception:
             await self.opinion_repository.db.rollback()
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+            logger.exception("Unexpected error while updating a new opinion")
+            raise bad_request(detail="Error updating opinion")
 
     async def get_rejected_opinions(self) -> list[Opinion]:
         """
