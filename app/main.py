0d5507faae7ca066f398303eefaaa6543d54152d
Yohan Grondin <yohan.grondin@outlook.com>
2026-06-18 10:03:34 +0200

    chore(app): update main configuration

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import app.core.logging
+from app.core.middlewares.auth_middleware import register_middlewares
 from app.api.v1.endpoints import (user_router,
                                   brand_router,
                                   car_router,
@@ -9,6 +10,8 @@ from app.api.v1.endpoints import (user_router,
                                   )
 
 app = FastAPI()
+
+register_middlewares(app)
 app.include_router(user_router)
 app.include_router(brand_router)
 app.include_router(car_router)
